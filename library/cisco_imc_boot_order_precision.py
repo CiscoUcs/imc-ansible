9b601af844cb1b5b5d1aa43433b1d2025a3a1fb8
vvb <vvb@cisco.com>
2017-01-18 21:20:12 +0530

    adding reapply and fixing a input param issue with reboot_on_update
    Signed-off-by: vvb <vvb@cisco.com>

--- library/cisco_imc_boot_order_precision.py
+++ library/cisco_imc_boot_order_precision.py
@@ -116,8 +116,8 @@ def boot_order_precision(server, module):
 
         set_boot_order(handle=server,
                        boot_devices=ansible['boot_devices'],
-                       reboot_on_update=ansible['reboot_on_update'],
-                       # reapply=ansible['reapply'],
+                       reboot_on_update=(False, True)[ansible['reboot_on_update'] == "yes"],
+                       reapply=(False, True)[ansible['reapply'] == "yes"],
                        configured_boot_mode=ansible['configured_boot_mode'],
                        server_id=ansible['server_id'])
 
